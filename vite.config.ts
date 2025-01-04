3d9a65d2bd936749157b112e9aee58cc7d2e566a
nera1 <nera4936@gmail.com>
2025-01-04 19:26:49 +0900

    feature: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-// https://vi
-// te.dev/config/
+
 export default defineConfig({
-  base: "/",
+  base: "",
   plugins: [react()],
   resolve: {
     alias: {
