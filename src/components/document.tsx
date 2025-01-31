e5f645c1e08212861aedefd5722be4fb7e602045
nera1 <nera4936@gmail.com>
2025-01-31 23:46:33 +0900

    feature: add favicon to link

--- src/components/document.tsx
+++ src/components/document.tsx
@@ -1,11 +1,33 @@
 import { useReactToPrint } from "react-to-print";
-import { useRef, useImperativeHandle, forwardRef } from "react";
+import {
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  FunctionComponent,
+  useEffect,
+  useState,
+} from "react";
 
 import "../styles/document.css";
 
 export interface PortfolioRef {
   toPrint: () => void;
 }
+
+export interface LinkTag {
+  href: string;
+  tag: string;
+  favicon?: string;
+}
+
+const LinkTag: FunctionComponent<LinkTag> = ({ href, tag, favicon }) => {
+  return (
+    <label>
+      <img width={20} height={20} src={favicon} />
+      <a href={href}>{tag}</a>
+    </label>
+  );
+};
 
 const MyDocument = forwardRef<PortfolioRef>((_, ref) => {
   const contentRef = useRef<HTMLDivElement>(null);
@@ -23,21 +45,32 @@ const MyDocument = forwardRef<PortfolioRef>((_, ref) => {
         <div ref={contentRef} className="canvas">
           <div className="flex gap-x-10">
             <img
-              className="object-cover rounded-none w-[180px] h-[180px]"
+              className="object-cover rounded-full w-[180px] h-[180px]"
               src="https://github.com/shadcn.png"
               alt="nera"
             />
-            <div className="grow info">
-              <p className="text-3xl font-bold">엄인용</p>
+            <div className="flex flex-col justify-center grow info">
+              <p className="text-2xl font-bold">엄인용</p>
               <ul className="text-sm font-medium">
                 <li>
                   <label>
-                    Blog.<a href="https://sumr.it">sumr.it</a>
+                    Blog.
+                    <LinkTag
+                      href="https://sumr.it"
+                      tag="sumr.it"
+                      favicon="https://sumr.it/favicon.ico"
+                    />
                   </label>
                 </li>
                 <li>
                   <label>
-                    Github.<a href="https://github.com/nera1">Nera</a>
+                    Github.
+                    <LinkTag
+                      href="https://github.com/nera1"
+                      tag="nera"
+                      favicon="https://github.githubassets.com/favicons/favicon.svg
+"
+                    />
                   </label>
                 </li>
                 <li>
