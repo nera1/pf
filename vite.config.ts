b6a115e140dbd569ea2e6029cd3ecff9d57f9709
nera1 <nera4936@gmail.com>
2025-08-19 21:52:13 +0900

    test: add build option to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: { outDir: "dist", emptyOutDir: true },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
